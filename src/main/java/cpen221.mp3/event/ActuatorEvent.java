83e1426d946d9b05e6723643ff749a539f205194
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2023-11-13 22:27:45 -0800

    richer events

--- src/main/java/cpen221.mp3/event/ActuatorEvent.java
+++ src/main/java/cpen221.mp3/event/ActuatorEvent.java
@@ -2,4 +2,44 @@ package cpen221.mp3.event;
 
 public class ActuatorEvent implements Event {
     // TODO: Implement this class
+    // you can add private fields and methods to this class
+
+    public ActuatorEvent(double TimeStamp, 
+                        int ClientId,
+                        int EntityId, 
+                        String EntityType, 
+                        boolean Value) {
+        // Implement this constructor
+    }
+
+    public double getTimeStamp() {
+        // Implement this method
+        return 0;
+    }
+
+    public int getClientId() {
+        // Implement this method
+        return 0;
+    }
+
+    public int getEntityId() {
+        // Implement this method
+        return 0;
+    }
+
+    public String getEntityType() {
+        // Implement this method
+        return null;
+    }
+
+    public boolean getValueBoolean() {
+        // Implement this method
+        return false;
+    }
+
+    // Actuator events do not have a double value
+    // no need to implement this method
+    public double getValueDouble() {
+        return -1;
+    }
 }
--- src/main/java/cpen221.mp3/event/SensorEvent.java
+++ src/main/java/cpen221.mp3/event/SensorEvent.java
@@ -1,9 +1,45 @@
 package cpen221.mp3.event;
 
 public class SensorEvent implements Event {
-    // Implement this class
+    // TODO: Implement this class
+    // you can add private fields and methods to this class
 
-    public SensorEvent(int entityId, String entityType, double valueDouble) {
+    public SensorEvent(double TimeStamp,
+                        int ClientId,
+                        int EntityId, 
+                        String EntityType, 
+                        double Value) {
         // Implement this constructor
+    }
+
+    public double getTimeStamp() {
+        // Implement this method
+        return 0;
+    }
+
+    public int getClientId() {
+        // Implement this method
+        return 0;
+    }
+
+    public int getEntityId() {
+        // Implement this method
+        return 0;
+    }
+
+    public String getEntityType() {
+        // Implement this method
+        return null;
+    }
+
+    public double getValueDouble() {
+        // Implement this method
+        return 0;
+    }
+
+    // Sensor events do not have a boolean value
+    // no need to implement this method
+    public boolean getValueBoolean() {
+        return false;
     }
 }
