5d70e0c3eaf01f6d7534ea75f41ad008183161b3
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2023-11-13 22:25:56 -0800

    richer filters

--- src/main/java/cpen221.mp3/filter/Filter.java
+++ src/main/java/cpen221.mp3/filter/Filter.java
@@ -1,6 +1,9 @@
 package cpen221.mp3.filter;
 
-enum Operator {
+import cpen221.mp3.event.Event;
+import java.util.List;
+
+enum DoubleOperator {
     EQUALS,
     GREATER_THAN,
     LESS_THAN,
@@ -8,56 +11,59 @@ enum Operator {
     LESS_THAN_OR_EQUALS
 }
 
+enum BooleanOperator {
+    EQUALS,
+    NOT_EQUALS
+}
+
 public class Filter {
+    // you can add private fields and methods to this class
 
     /**
-     * Constructs a filter that matches boolean events with the given boolean value.
-     * For non-boolean events, the satisfies method should return false.
+     * Constructs a filter that compares the boolean (actuator) event value
+     * with the given boolean value using the given operator.
+     * An Operator can be one of the following:
+     * 
+     * BooleanOperator.EQUALS
+     * BooleanOperator.NOT_EQUALS
      *
      * @param value the boolean value to match
+     * @param operator the BooleanOperator to use to compare the event value with the given value
      */
-    public Filter(boolean value) {
-        // constructor
+    public Filter(boolean value, BooleanOperator operator) {
+        // TODO: implement this method
     }
 
     /**
-     * Constructs a filter that matches double events with the given double value.
-     * It uses the given operator to compare the double value of the event with the given value.
-     * An Operator can be one of the following:
+     * Constructs a filter that compares a double field in events
+     * with the given double value using the given DoubleOperator.
+     * An DoubleOperator can be one of the following:
      * 
-     * Operator.EQUALS
-     * Operator.GREATER_THAN
-     * Operator.LESS_THAN
-     * Operator.GREATER_THAN_OR_EQUALS
-     * Operator.LESS_THAN_OR_EQUALS
+     * DoubleOperator.EQUALS
+     * DoubleOperator.GREATER_THAN
+     * DoubleOperator.LESS_THAN
+     * DoubleOperator.GREATER_THAN_OR_EQUALS
+     * DoubleOperator.LESS_THAN_OR_EQUALS
      * 
      * For non-double (boolean) value events, the satisfies method should return false.
      *
+     * @param field the field to match (event "value" or event "timestamp")
      * @param value the double value to match
-     * @param operator the operator to use to compare the event value with the given value
+     * @param operator the DoubleOperator to use to compare the event value with the given value
      */
-    public Filter(double value, Operator operator) {
-        // constructor
-    }
-
-    /**
-     * Constructs a filter that comapres event TimeStamp with the given TimeStamp.
-     *
-     * @param TimeStamp the TimeStamp to compare with
-     * @param operator the operator to use for comparison
-     */
-    public Filter(double TimeStamp, Operator operator) {
-        // constructor
+    public Filter(String field, double value, DoubleOperator operator) {
+        // TODO: implement this method
     }
     
     /**
+     * A filter can be composed of other filters.
+     * in this case, the filter should satisfy all the filters in the list.
      * Constructs a complex filter composed of other filters.
      *
      * @param filters the list of filters to use in the composition
      */
     public Filter(List<Filter> filters) {
-        // a filter can be composed of other filters
-        // in this case, the filter should satisfy all the filters in the list
+        // TODO: implement this method
     }
 
     /**
@@ -67,7 +73,7 @@ public class Filter {
      * @return true if the event satisfies the filter criteria, false otherwise
      */
     public boolean satisfies(Event event) {
-        // implement this method
+        // TODO: implement this method
         return false;
     }
 
@@ -78,7 +84,7 @@ public class Filter {
      * @return true if every event in the list satisfies the filter criteria, false otherwise
      */
     public boolean satisfies(List<Event> events) {
-        // implement this method
+        // TODO: implement this method
         return false;
     }
 
@@ -90,7 +96,7 @@ public class Filter {
      * @return a new event if it satisfies the filter criteria, null otherwise
      */
     public Event sift(Event event) {
-        // implement this method
+        // TODO: implement this method
         return null;
     }
 
@@ -103,7 +109,7 @@ public class Filter {
      *        or an empty list if no events in the given list satisfy the filter criteria
      */
     public List<Event> sift(List<Event> events) {
-        // implement this method
+        // TODO: implement this method
         return null;
     }
 }
