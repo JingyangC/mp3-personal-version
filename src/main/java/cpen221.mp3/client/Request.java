a0b20ff266c7b529719b13dbbad3a06940d8fb44
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2023-11-16 16:45:32 -0800

    minor changes

--- src/main/java/cpen221.mp3/client/Request.java
+++ src/main/java/cpen221.mp3/client/Request.java
@@ -1,22 +1,5 @@
 package cpen221.mp3.client;
 
-enum RequestType {
-    CONFIG,
-    CONTROL,
-    ANALYSIS
-}
-
-enum RequestCommand {
-    CONFIG_UPDATE_MAX_WAIT_TIME,
-    CONTROL_SET_ACTUATOR_STATE,
-    CONTROL_TOGGLE_ACTUATOR_STATE,
-    CONTROL_NOTIFY,
-    ANALYSIS_GET_EVENTS_IN_WINDOW,
-    ANALYSIS_GET_ALL_ENTITIES,
-    ANALYSIS_GET_LATEST_EVENTS,
-    ANALYSIS_GET_MOST_ACTIVE_ENTITY,
-}
-
 public class Request {
     private final double timeStamp;
     private final RequestType requestType;
---
+++ src/main/java/cpen221.mp3/client/RequestType.java
@@ -0,0 +1,7 @@
+package cpen221.mp3.client;
+
+public enum RequestType {
+    CONFIG,
+    CONTROL,
+    ANALYSIS
+}
